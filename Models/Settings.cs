4b74819ad4f095cb4cd9c1a16910d69af94357cb
Tsukasa <admin@tsukasaze.com>
2025-08-26 09:06:46 +0900

    Add files via upload

--- Models/Settings.cs
+++ Models/Settings.cs
@@ -11,5 +11,12 @@ namespace Sympho.Models
         public int MaxSpamPerInterval { get; set; } = 10;
         public float SpamCheckInterval { get; set; } = 20.0f;
         public float AntiSpamCooldown { get; set; } = 60.0f;
+        
+        public string TmpDir { get; set; } = "temp";
+        public int TmpMaxAgeMinutes { get; set; } = 30;
+        public int CacheMaxCount { get; set; } = 100;
+        public bool CacheReuseEnabled { get; set; } = true;
+        public bool QueueEnabled { get; set; } = false;
+        public int MaxQueueLength { get; set; } = 10;
     }
 }
